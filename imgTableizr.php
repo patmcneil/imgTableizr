04638d87d442cf64363949f259f32f86b1a98599
Patrick <pmcneil18@gmail.com>
2015-10-16 11:02:52 -0400

    Updating the renamed file. Forgot to pull from server...

--- imgTableizr.php
+++ imgTableizr.php
@@ -3,7 +3,8 @@
 # imgTableizr v 1.0 
 # Patrick McNeil
 # http://patmcneil.net
-# Updated: Oct. 15, 2015
+
+imgTableizr('profile.jpg');
 
 function imgTableizr( $imgSrc, $quality='medium', $width='no-resize' ){
 
@@ -12,28 +13,29 @@ function imgTableizr( $imgSrc, $quality='medium', $width='no-resize' ){
 	$output = array();
 
 	$explodedSrc = explode( '.', $imgSrc );
-	$ext = $explodedSrc[ count($explodedSrc) - 1 ];
+	$ext = end( $explodedSrc );
 
-	if (preg_match('/jpg|jpeg/i',$ext)){
-		$img = imagecreatefromjpeg( $imgSrc );
-	}else if (preg_match('/png/i',$ext)){
-		$img = imagecreatefrompng( $imgSrc );
-	}else if (preg_match('/gif/i',$ext)){
-		$img = imagecreatefromgif( $imgSrc );
-	}else if (preg_match('/bmp/i',$ext)){
-		$img = imagecreatefrombmp( $imgSrc );
-	}else{
-		return false;
+	switch( $ext ){
+		case 'jpg' || 'jpeg':
+			$img = imagecreatefromjpeg( $imgSrc );
+		break;
+		case 'png':
+			$img = imagecreatefrompng( $imgSrc );
+		break;
+		case 'gif':
+			$img = imagecreatefromgif( $imgSrc );
+		break;
+		case 'bmp':
+			$img = imagecreatefrombmp( $imgSrc );
+		break;
+		default: return false;
 	}
 
 	if( $width != 'no-resize' && is_int( $width ) && $width > 0 ){
 		$img = imagescale( $img, $width );
 	}    
 
-	$imgW = imagesx( $img );
-	$imgH = imagesy( $img );
-
-	switch($quality){
+	switch( $quality ){
 		case 'medium':
 			$threshold = 20;
 		break;
@@ -53,31 +55,31 @@ function imgTableizr( $imgSrc, $quality='medium', $width='no-resize' ){
 			$threshold = 0;
 		break;
 		default: return false;
-	}	
+	}
 
-	array_push( $output,'<table style="border-spacing:0px;width:'.$imgW.'px;height:'.$imgH.'px;margin:0;padding:0;border-spacing:0;">' );
+	$imgW = imagesx( $img );
+	$imgH = imagesy( $img );
 
-	for( $row = 0; $imgH > $row; $row++ ){
+	array_push( $output,'<table style="border-spacing:0;width:'.$imgW.'px;height:'.$imgH.'px;margin:0;padding:0;">' );
 
-		array_push( $output,'<tr>' );
+	for( $y = 0; $imgH > $y; $y++ ){
 
-		for( $col = 0; $imgW > $col; $col++ ){
+		array_push( $output, '<tr>' );
 
-			$rgb = imagecolorat( $img, $col, $row );
-			$r = ( $rgb >> 16 ) & 0xFF;
-			$g = ( $rgb >> 8 ) & 0xFF;
-			$b = $rgb & 0xFF;
-			$currentRGB = array( 0 => $r, 1 => $g, 2 => $b) ;
+		for( $x = 0; $imgW > $x; $x++ ){
+
+			$rgb = imagecolorat( $img, $x, $y );
+			$currentRGB = array( 0 => ( $rgb >> 16 ) & 0xFF, 1 => ( $rgb >> 8 ) & 0xFF, 2 => $rgb & 0xFF);
 			$currentHex = rgb2hex($currentRGB);
 			
-			if( $col != 0 ){
+			if( $x != 0 ){
 				if( abs( $currentRGB[0] - $prevRGB[0]) <= $threshold ){
 					if( abs( $currentRGB[1] - $prevRGB[1]) <= $threshold ){
 						if( abs( $currentRGB[2] - $prevRGB[2]) <= $threshold ){
 
-							$colspan ++;
+							$colspan++;
 
-							if( $col+1 == $imgW ){
+							if( $x + 1 == $imgW ){
 								generateCell();
 							}
 						}else{
@@ -94,15 +96,13 @@ function imgTableizr( $imgSrc, $quality='medium', $width='no-resize' ){
 				$prevRGB = $currentRGB;
 			}
 		}
-		array_push( $output,'</tr>' );
+		array_push( $output, '</tr>' );
 	}
 	array_push( $output, '</table>');
 
-	$output = implode( '', $output );
-
 	imagedestroy( $img );
 
-	return $output;
+	echo implode( '', $output );;
 }
 
 function rgb2hex($rgb) {
@@ -119,3 +119,4 @@ function generateCell(){
 	$prevHex = $currentHex;
 	$prevRGB = $currentRGB;
 	$colspan = 1;
+}
